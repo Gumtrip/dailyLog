8ea5ae9eec6fa29bff5d053785c81b324b412c83
gumtrip <gumtrip@126.com>
2019-07-19 16:53:17 +0800

    首页

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <title>mission</title>
+    <link href={{asset('/static/css/app.5da6c378843d6a767179d21a15e1b6d4.css')}} rel=stylesheet>
+</head>
+<body>
+<div id=app></div>
+<script type=text/javascript src={{asset('/static/js/manifest.de03f16a39bf93f79b51.js')}}></script>
+<script type=text/javascript src={{asset('/static/js/vendor.b42fc7d0977c6b837925.js')}}></script>
+<script type=text/javascript src={{asset('/static/js/app.3eca0968ecc1f6b258fc.js')}}></script>
+</body>
+</html>
