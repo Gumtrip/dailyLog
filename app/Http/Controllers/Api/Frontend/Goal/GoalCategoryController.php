634a3cc8a8081c38f974230091280d128c8f1dd7
gumtrip <gumtrip@126.com>
2019-07-22 10:07:38 +0800

    增加了目标分类

---
+++ app/Http/Controllers/Api/Frontend/Goal/GoalCategoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Frontend\Goal;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Controller;
+
+class GoalCategoryController extends Controller
+{
+    //
+}
