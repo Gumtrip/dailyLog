8ba969f27e5ff42345dd910f9db43abb89e23c5e
gumtrip <gumtrip@126.com>
2019-07-24 09:53:47 +0800

    去除mission_los mission_id user_id 外键约束

--- database/migrations/2019_07_05_151637_create_mission_logs_table.php
+++ database/migrations/2019_07_05_151637_create_mission_logs_table.php
@@ -16,10 +16,8 @@ class CreateMissionLogsTable extends Migration
         Schema::create('mission_logs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedInteger('mission_id');
-            $table->foreign('mission_id')->references('id')->on('missions')->onDelete('cascade');
             $table->string('description',150)->nullable()->comment('描述');
             $table->unsignedBigInteger('user_id')->nullable();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->text('properties')->nullable();
             $table->timestamps();
         });
