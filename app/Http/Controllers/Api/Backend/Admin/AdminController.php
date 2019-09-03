09a12826898bc80e0783827e84ea484f43145868
gumtrip <gumtrip@126.com>
2019-09-03 15:23:56 +0800

    添加了admin

---
+++ app/Http/Controllers/Api/Backend/Admin/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Backend\Admin;
+
+use App\Http\Controllers\Api\Controller;
+use App\Http\Requests\Backend\BackendRequest as Request;
+
+
+
+class AdminController extends Controller
+{
+    //
+}
---
+++ app/Transformers/Backend/Admin/AdminTransformer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Transformers\Backend\Admin;
+
+use League\Fractal\TransformerAbstract;
+
+class AdminTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform()
+    {
+        return [
+            //
+        ];
+    }
+}
